1cba93ae13799514149dd427ede77e03ee5dade8
Dat Nguyen Trong <DatNT4@fpts.com.vn>
2024-09-05 15:41:46 +0700

    feat: add change tracker

--- razor-impl/Context/MovieDbContext.cs
+++ razor-impl/Context/MovieDbContext.cs
@@ -10,4 +10,35 @@ public class MovieDbContext : DbContext
     }
 
     public DbSet<razor_impl.Models.Movie>? Movie { get; set; }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries())
+        {
+            var key = entry.Metadata.FindPrimaryKey();
+
+            if (key != null)
+            {
+                var keyValues = key.Properties.Select(p => entry.Property(p.Name).CurrentValue).ToArray();
+
+                var keyValueString = string.Join(", ", keyValues);
+                Console.WriteLine($"Entity: {entry.Entity.GetType().Name}, State: {entry.State}, Key: {keyValueString}");
+            }
+
+            if (entry.State == EntityState.Added)
+            {
+
+            }
+            else if (entry.State == EntityState.Modified)
+            {
+
+            }
+            else if (entry.State == EntityState.Deleted)
+            {
+
+            }
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
 }
